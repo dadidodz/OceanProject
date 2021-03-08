f6fee82505e3d3a3628e89b33fa141d566333956
Tom Plelo <tomplelo@caramail.com>
2021-03-08 16:58:45 +0100

    Map

--- app/src/main/java/com/example/oceanprotect/MainActivity.java
+++ app/src/main/java/com/example/oceanprotect/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Map.class));
+            }
+        });
     }
 
     @Override
@@ -49,4 +60,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
---
+++ app/src/main/java/com/example/oceanprotect/Map.java
@@ -0,0 +1,18 @@
+package com.example.oceanprotect;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_layout);
+
+    }
+}
