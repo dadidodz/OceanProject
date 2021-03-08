29c13f813f1188e7c3e97315a9f8efc817606e35
Dorian BLATIERE <dorian.blatiere@hotmail.fr>
2021-03-08 15:39:46 +0100

    08/03/2020 3eme commit (nouvelle activité menu)

---
+++ app/src/main/java/com/example/oceanprotect/Favoris.java
@@ -0,0 +1,34 @@
+package com.example.oceanprotect;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Favoris extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favoris_layout);
+
+        // Récupère le paramètre de l'intent via la clé utilisée
+        //String texte = getIntent().getStringExtra("letexte");
+
+        // modifie le texte du label de l'activité avec cette valeur
+        //TextView textV = (TextView)findViewById(R.id.textView);
+        //textV.setText(texte);
+
+        // Mise en place du traitement sur le bouton ...
+        Button boutonRetour = (Button)findViewById(R.id.buttonrtn);
+
+        boutonRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/oceanprotect/MainActivity.java
+++ app/src/main/java/com/example/oceanprotect/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.oceanprotect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +23,31 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflaterMenu = getMenuInflater();
         inflaterMenu.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.favori:
+                Intent intent = new Intent(MainActivity.this, Favoris.class);
+
+                // Récupère la valeur de l'item à la position sur laquelle on a cliqué
+                //String valeurItem = (String) parent.getItemAtPosition(position);
+
+                // Fixe un paramètre sous la forme clé-valeur
+                //intent.putExtra("letexte", valeurItem);
+
+                //Toast.makeText(MainActivity.this, "bonjour", Toast.LENGTH_SHORT).show();
+
+                startActivity(intent);
+
+                break;
+            case R.id.quitmenu:
+                System.exit(0) ;
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
