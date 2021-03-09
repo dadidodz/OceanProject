0919187f866434558676b6321b6f58677c5efdc8
Tom Plelo <tomplelo@caramail.com>
2021-03-09 12:22:29 +0100

    Database

---
+++ app/src/main/java/com/example/oceanprotect/Database.java
@@ -0,0 +1,33 @@
+package com.example.oceanprotect;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Ocean.db";
+    public static final String TABLE_NAME = "ocean_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "DESCRIPTION";
+    public static final String COL_4 = "INFORMATION";
+
+    public Database(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, DESCRIPTION TEXT, INFORMATION TEXT) ");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
